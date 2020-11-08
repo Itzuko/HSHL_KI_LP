6f3c562dda0f7056e961e952a819c04008dc8c56
PierreSucker <49077286+PierreSucker@users.noreply.github.com>
2020-11-08 17:52:12 +0100

    added comments and output of file

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import os
 import sys
-startImage = ("""  ______ ______ ______ ______ ______ ______ ______ ______ ______ ______ 
+startImage = ("""\
+  ______ ______ ______ ______ ______ ______ ______ ______ ______ ______ 
  |______|______|______|______|______|______|______|______|______|______|
   _    _  _____ _    _ _
  | |  | |/ ____| |  | | |                                               
@@ -21,31 +22,36 @@ startImage = ("""  ______ ______ ______ ______ ______ ______ ______ ______ _____
  | |   | |  __/ |  | | |  __/  ____) | |_| | (__|   <  __/ |            
  |_|   |_|\___|_|  |_|  \___| |_____/ \__,_|\___|_|\_\___|_|            
   ______ ______ ______ ______ ______ ______ ______ ______ ______ ______ 
- |______|______|______|______|______|______|______|______|______|______""")
+ |______|______|______|______|______|______|______|______|______|______|""")
 print(startImage)
 print("\nWillkommen")
-print("Folgende Probleme könne gelöst werden")
-#Array for Files
+print("Folgende Probleme können gelöst werden")
+#Array für die Dateien
 fileListName = []
 fileListPath = []
 
+#Dateien Öffnen und zu den Array hinzufügen
 d = 'KI_Benchmarks'
 for path in os.listdir(d):
     full_path = os.path.join(d, path)
     if os.path.isfile(full_path):
-        fileListPath.append(full_path.split("\\")[0])
+        fileListPath.append(full_path)
         fileListName.append(full_path.split("\\")[1])
 
-
+#Anzeigen zwischen den Optionen
 tmpCounter = 0
 for fileName in fileListName:
     print( "[%d] %s\r" %(tmpCounter, fileName))
     tmpCounter = tmpCounter + 1
 
-#Userinput
+#Auswahl vom User der Dateien
 choice = input("\n\rWelches Problem soll gelöst werden? [0 - " + str(tmpCounter-1) + " ]: ")
-
 print("Es wurde gewählt", fileListName[int(choice)])
-
-
+#Öffnen und Ausgabe der Datei
+f = open(fileListPath[int(choice)])
+readFile = f.read()
+print(readFile)
+#Anzahl der Constraints
+lineCount = readFile.splitlines()
+print(len(lineCount) - 3)
 
