7f3b283a25ce0b707d414cddfa0af793a78d42dd
PierreSucker <Pierresucker@hotmail.de>
2021-07-19 18:39:35 +0200

    working restart and exit

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ import numpy as np
 import re
 import startProgramm as start
 import simplex
+import os
 #Lösung überprüft mit:
 #http://simplex.tode.cz/en/ 
 
 #Eigenes Github-Repo:
 #https://github.com/Itzuko/HSHL_KI_LP
 
+
 start.start()
 start.showKIProblems()
-usersChoice =  start.getUserInput()
+usersChoice = start.getUserInput()
 simplex.startSimplex(usersChoice)
+print("----------------------------")
+choice = input(
+    "\n\rRun again? (y/n)\n")
+choice = choice.lower()
+if(choice == 'y'):
+    def clear(): return os.system('cls')
+    clear()
+    os.system("python main.py")
+    print("Restarting...")
+    exit()
+else:
+    input("Press ENTER to exit")
+
