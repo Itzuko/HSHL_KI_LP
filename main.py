c0fe518a7e8e4b705675ffea36e3083a200eaa58
PierreSucker <Pierresucker@hotmail.de>
2021-07-19 20:31:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import re
 import startProgramm as start
 import simplex
 import os
-#Lösung überprüft mit:
-#http://simplex.tode.cz/en/ 
 
 #Eigenes Github-Repo:
 #https://github.com/Itzuko/HSHL_KI_LP
